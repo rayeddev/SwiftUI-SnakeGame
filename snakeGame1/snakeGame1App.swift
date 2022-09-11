bb3be935c65637424e798bf0b64379a021d49d05
Rayed Ali <rayeddev@gmail.com>
2022-09-11 10:32:08 +0300

    Initial Commit

---
+++ snakeGame1/snakeGame1App.swift
@@ -0,0 +1,17 @@
+//
+//  snakeGame1App.swift
+//  snakeGame1
+//
+//  Created by RAYED AL NOOM on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct snakeGame1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
